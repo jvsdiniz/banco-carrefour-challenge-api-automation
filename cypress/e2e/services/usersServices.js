e6a4bf2f11f354fbd6a26b7c3ed7e0544c4b9980
João Diniz <joao.diniz@sptech.school>
2025-09-23 23:08:21 -0300

    primeiros cenários

---
+++ cypress/e2e/services/usersServices.js
@@ -0,0 +1,273 @@
+import { faker } from '@faker-js/faker';
+import * as utils from "../../support/utils"
+
+class usersServices {
+
+    gerarDadosNovoUsuario() {
+        return {
+            nome: faker.person.fullName(),
+            email: faker.internet.email(),
+            senha: faker.internet.password({ length: 8 })
+        }
+    }
+
+    gerarDadosCamposVazios() {
+        return {
+            nome: "",
+            email: "",
+            password: "",
+            administrador: ""
+        }
+    }
+
+    gerarId() {
+        let id = utils.gerarId();
+        return cy.wrap(id);
+    }
+
+    cadastrarNovoUsuario() {
+        const usuario = this.gerarDadosNovoUsuario()
+
+        return cy.api({
+            method: 'POST',
+            url: `${Cypress.env("apiUrl")}/usuarios`,
+            body: {
+                "nome": usuario.nome,
+                "email": usuario.email,
+                "password": usuario.senha,
+                "administrador": "true"
+            }
+        })
+    }
+
+    validarCadastroNovoUsuario() {
+        cy.get('@novoUsuario').then((response) => {
+            expect(response.status).to.eq(201);
+            expect(response.body.message).to.eq("Cadastro realizado com sucesso");
+            expect(response.body._id).to.exist
+        })
+        cy.screenshot();
+    }
+
+    buscarTodosOsUsuarios() {
+        return cy.api({
+            method: 'GET',
+            url: `${Cypress.env("apiUrl")}/usuarios`
+        })
+    }
+
+    validarBuscarTodosOsUsuarios() {
+        cy.get('@buscaTodosOsUsuarios').then((response) => {
+            const { quantidade, usuarios } = response.body;
+
+            expect(quantidade).to.be.a('number');
+            expect(Number.isInteger(quantidade)).to.be.true;
+            expect(usuarios).to.be.an('array');
+            expect(usuarios.length).to.equal(quantidade);
+
+            if (quantidade > 0) {
+                expect(usuarios.length).to.be.greaterThan(0);
+            }
+
+            if (usuarios.length > 0) {
+                const primeiroUsuario = usuarios[0];
+                expect(primeiroUsuario).to.have.all.keys('nome', 'email', 'password', 'administrador', '_id');
+            }
+
+        })
+        cy.screenshot();
+    }
+
+    buscarUsuarioAtravesDoRegistro() {
+        return cy.get('@usuario').then((usuario) => {
+            const id = usuario.body._id;
+
+            return cy.api({
+                method: 'GET',
+                url: `${Cypress.env("apiUrl")}/usuarios/${id}`,
+                failOnStatusCode: false
+            })
+        })
+    }
+
+    validarBuscarUsuarioAtravesDoRegistro() {
+        cy.get('@buscarUsuario').then((response) => {
+            const usuario = response.body;
+
+            expect(response.status).to.eq(200);
+
+            expect(usuario).to.have.all.keys('nome', 'email', 'password', 'administrador', '_id');
+            expect(usuario.nome).to.be.a('string');
+            expect(usuario.email).to.be.a('string');
+            expect(usuario.password).to.be.a('string');
+            expect(usuario.administrador).to.be.a('string');
+            expect(usuario.administrador).to.be.oneOf(['true', 'false']);
+            expect(usuario._id).to.be.a('string');
+            expect(usuario.email).to.include('@');
+        })
+        cy.screenshot();
+    }
+
+    atualizarDadosUsuario(level) {
+        return cy.get('@buscarUsuario').then((usuario) => {
+            const id = usuario.body._id
+            const usuarioExistente = usuario.body
+
+            if (level == 'email') {
+                let novoEmail = this.gerarDadosNovoUsuario().email;
+                usuarioExistente.email = novoEmail;
+            } else if (level == 'nome') {
+                let novoNome = this.gerarDadosNovoUsuario().nome;
+                usuarioExistente.nome = novoNome
+            } else if (level == 'password') {
+                let novoPassword = this.gerarDadosNovoUsuario().senha;
+                usuarioExistente.password = novoPassword;
+            }
+            delete usuarioExistente._id;
+
+            return cy.api({
+                method: 'PUT',
+                url: `${Cypress.env("apiUrl")}/usuarios/${id}`,
+                body: usuarioExistente
+            })
+        })
+    }
+
+    validarAtualizarDadosUsuario() {
+        cy.get('@usuarioAtualizado').then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.message).to.eq("Registro alterado com sucesso");
+        })
+        cy.screenshot();
+    }
+
+    deletarUsuario() {
+        return cy.get('@buscarUsuario').then((usuario) => {
+            const id = usuario.body._id;
+
+            cy.api({
+                method: 'DELETE',
+                url: `${Cypress.env("apiUrl")}/usuarios/${id}`,
+                failOnStatusCode: false
+            })
+        })
+    }
+
+    validarUsuarioInexistenteNaBase() {
+        cy.get('@buscarUsuario').then((response) => {
+            expect(response.status).to.eq(400);
+            expect(response.body.message).to.eq("Usuário não encontrado");
+        })
+        cy.screenshot();
+    }
+
+    buscarUsuarioComRegistroInvalido() {
+        return cy.api({
+            method: 'GET',
+            url: `${Cypress.env("apiUrl")}/usuarios/usuarioInexistente`,
+            failOnStatusCode: false
+        })
+    }
+
+    validarUsuarioComRegristoInvalido() {
+        cy.get('@usuarioInvalido').then((response) => {
+            expect(response.status).to.eq(400);
+            expect(response.body.id).to.eq("id deve ter exatamente 16 caracteres alfanuméricos");
+        })
+        cy.screenshot();
+    }
+
+    cadastrarUsuarioCamposVazios() {
+        const usuarioCamposVazios = this.gerarDadosCamposVazios();
+
+        return cy.api({
+            method: 'POST',
+            url: `${Cypress.env("apiUrl")}/usuarios`,
+            body: usuarioCamposVazios,
+            failOnStatusCode: false
+        })
+    }
+
+    validarUsuarioCamposVazios() {
+        cy.get('@usuarioCamposVazios').then((response) => {
+            const body = response.body;
+            expect(response.status).to.eq(400);
+
+            expect(body.nome).to.eq("nome não pode ficar em branco");
+            expect(body.email).to.eq("email não pode ficar em branco");
+            expect(body.password).to.eq("password não pode ficar em branco");
+            expect(body.administrador).to.eq("administrador deve ser 'true' ou 'false'");
+        })
+        cy.screenshot();
+    }
+
+    cadastrarUsuarioBodyVazio() {
+        const usuarioBodyVazio = utils.gerarBodyVazio();
+
+        return cy.api({
+            method: 'POST',
+            url: `${Cypress.env("apiUrl")}/usuarios`,
+            body: usuarioBodyVazio,
+            failOnStatusCode: false
+        })
+    }
+
+    validarUsuarioBodyVazio() {
+        cy.get('@usuarioBodyVazio').then((response) => {
+            const body = response.body;
+            expect(response.status).to.eq(400);
+
+            expect(body.nome).to.eq("nome é obrigatório");
+            expect(body.email).to.eq("email é obrigatório");
+            expect(body.password).to.eq("password é obrigatório");
+            expect(body.administrador).to.eq("administrador é obrigatório");
+        })
+        cy.screenshot();
+    }
+
+    atualizarUsuarioComRegistro() {
+        return cy.get('@buscarUsuario').then((usuario) => {
+            const id = usuario.body._id
+            const usuarioExistente = usuario.body
+            const novoId = utils.gerarId();
+
+            usuarioExistente._id = novoId;
+
+            return cy.api({
+                method: 'PUT',
+                url: `${Cypress.env("apiUrl")}/usuarios/${id}`,
+                body: usuarioExistente,
+                failOnStatusCode: false
+            })
+        })
+    }
+
+    validarIdNaoPermitido() {
+        cy.get('@usuarioAtualizado').then((response) => {
+            expect(response.status).to.eq(400);
+            expect(response.body._id).to.eq("_id não é permitido")
+        })
+        cy.screenshot();
+    }
+
+    deletarUsuarioInexistente() {
+        return cy.get('@novoId').then((id) => { // agora é só string
+            return cy.api({
+                method: 'DELETE',
+                url: `${Cypress.env("apiUrl")}/usuarios/${id}`,
+                failOnStatusCode: false
+            });
+        });
+    }
+
+
+    validarDeletarUsuarioInexistente() {
+        cy.get('@usuarioExcluido').then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.message).to.eq("Nenhum registro excluído");
+        })
+        cy.screenshot();
+    }
+
+}
+export default usersServices;
---
+++ cypress/support/step_definitions/api/users.steps.js
@@ -0,0 +1,117 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import UsersServices from "../../../e2e/services/usersServices"
+import * as utils from "../../utils"
+
+const usersServices = new UsersServices();
+
+Given("que eu queira buscar a lista contendo todos os usuários", () => {
+    return true;
+});
+
+When("realizo a busca através da chamada GET", () => {
+    usersServices.buscarTodosOsUsuarios().as('buscaTodosOsUsuarios');
+});
+
+Then("deve ser retornado a lista com todos os usuarios", () => {
+    usersServices.validarBuscarTodosOsUsuarios();
+});
+
+Given("que eu queira cadastrar um novo usuario no sistema", () => {
+    usersServices.gerarDadosNovoUsuario();
+});
+
+When("realizo uma chamada POST para endpoint", () => {
+    usersServices.cadastrarNovoUsuario().as('novoUsuario');
+});
+
+Then("o usuario deve ser cadsatrado com sucesso", () => {
+    usersServices.validarCadastroNovoUsuario();
+});
+
+Given("que eu tenha um usuario cadastrado", () => {
+    usersServices.cadastrarNovoUsuario().as('usuario');
+});
+
+When("eu realizo a busca através do registro dele", () => {
+    usersServices.buscarUsuarioAtravesDoRegistro().as('buscarUsuario');
+});
+
+Then("deve ser retornado o usuario cadastrado", () => {
+    usersServices.validarBuscarUsuarioAtravesDoRegistro();
+});
+
+When("eu atualizar o {string} do usuario", (level) => {
+    usersServices.buscarUsuarioAtravesDoRegistro().as('buscarUsuario');
+    usersServices.atualizarDadosUsuario(level).as('usuarioAtualizado');
+});
+
+Then("deve ser retornado o usuario", () => {
+    usersServices.validarAtualizarDadosUsuario();
+});
+
+When("eu deletar o usuario existente", () => {
+    usersServices.buscarUsuarioAtravesDoRegistro().as('buscarUsuario');
+    usersServices.deletarUsuario();
+});
+
+Then("não deve ser possivel achar o usuario realizando busca", () => {
+    usersServices.buscarUsuarioAtravesDoRegistro().as('buscarUsuario');
+    usersServices.validarUsuarioInexistenteNaBase();
+});
+
+Given("que eu quero realizar uma busca com um id invalido", () => {
+    return true;
+});
+
+When("realizo a chamada com o id invalido", () => {
+    usersServices.buscarUsuarioComRegistroInvalido().as('usuarioInvalido')
+});
+
+Then("deve ser retornado a mensagem de erro id deve ter exatamente 16 caracteres alfanuméricos", () => {
+    usersServices.validarUsuarioComRegristoInvalido();
+});
+
+Given("que eu quero realizar um cadastro com os campos vazios", () => {
+    usersServices.gerarDadosCamposVazios();
+});
+
+When("realizo a chamada com todos os campos vazios", () => {
+    usersServices.cadastrarUsuarioCamposVazios().as('usuarioCamposVazios');
+});
+
+Then("deve ser retornado a mensagem que os campos nao podem ficar em branco", () => {
+    usersServices.validarUsuarioCamposVazios();
+});
+
+Given("o body esteja vazio", () => {
+    utils.gerarBodyVazio();
+});
+
+When("realizo a chamada sem enviar o body", () => {
+    usersServices.cadastrarUsuarioBodyVazio().as('usuarioBodyVazio');
+});
+
+Then("deve ser retornado que os campos são obrigatórios", () => {
+    usersServices.validarUsuarioBodyVazio();
+});
+
+When("eu atualizar o id do usuario", () => {
+    usersServices.buscarUsuarioAtravesDoRegistro().as('buscarUsuario');
+    usersServices.atualizarUsuarioComRegistro().as('usuarioAtualizado');
+});
+
+Then("nao deve ser permitido atualizar o id", () => {
+    usersServices.validarIdNaoPermitido();
+});
+
+Given("que eu tenha um registro invalido", () => {
+    usersServices.gerarId().as('novoId');
+});
+
+When("eu deletar o usuario", () => {
+    usersServices.deletarUsuarioInexistente().as('usuarioExcluido');
+});
+
+Then("nenhum registro deve ser excluído", () => {
+    usersServices.validarDeletarUsuarioInexistente();
+});
